9c903e2f74ea7fe56bf6b314a0a661dc2e92344c
Phanindra <phanindra2105@gmail.com>
2025-03-09 18:00:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ from ttkbootstrap.constants import *
 from tkinter import messagebox
 TASK_FILE = "tasks.json"
 
-def save_tasks(tasks):
-    """Save tasks to JSON file."""
-    with open(TASK_FILE, "w") as file:
-        json.dump(tasks, file, indent=4)
-
 def add_task():
     """Add a new task."""
     title = title_entry.get()
